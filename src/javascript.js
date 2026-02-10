0e1c60aa05480802c387ec89f87da09c3607373b
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-02-10 15:15:08 +0000

    Integration of moment .js and moment timezone and created functions in JS with intervals to display date and time every second

---
+++ src/javascript.js
@@ -0,0 +1,42 @@
+console.log(moment);
+console.log(moment.tz);
+
+function updateLondonTime() {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
+}
+
+updateLondonTime();
+setInterval(updateLondonTime, 1000);
+
+function updateNewYorkTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
+}
+
+updateNewYorkTime();
+setInterval(updateNewYorkTime, 1000);
+
+function updateTokyoTime() {
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTokyoTime();
+setInterval(updateTokyoTime, 1000);
