c2b0b4e3457be14d8b44f68b5214bca222843eb9
kimberleyjthornton-debug <kimberleyjthornton@gmail.com>
2026-02-10 16:08:55 +0000

    Added id's to various points of the select form for grabbing the data from JS.  JS has been instructed to redisplay the data inside the innerHTML as appropriate.

--- src/javascript.js
+++ src/javascript.js
@@ -40,3 +40,22 @@ function updateTokyoTime() {
 
 updateTokyoTime();
 setInterval(updateTokyoTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+   <div class="city">
+    <div>
+     <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+     <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
+    </div>`;
+}
+
+let citySelect = document.querySelector("#city");
+
+citySelect.addEventListener("change", updateCity);
